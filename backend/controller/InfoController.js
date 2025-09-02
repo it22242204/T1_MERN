639e6a839ea9442d1c48c7d5ca8edc0ee2aea45b
Ashokan <ashokankuganathan@gmail.com>
2025-09-02 07:07:53 +0530

    backend updated with crud

--- backend/controller/InfoController.js
+++ backend/controller/InfoController.js
@@ -1,5 +1,6 @@
 const infoModel = require ('../models/infomodel');
 
+// create info
 const createInfo = async (req,res) => {
     const {name, nested, age, address, phone, email, Birthdate, Married, Salary} = req.body;
 
@@ -11,4 +12,53 @@ const createInfo = async (req,res) => {
     }
 }
 
-module.exports = createInfo;
+// get all info
+const getAllInfo = async (req,res) => {
+    try{
+        const info = await infoModel.find({});
+        res.status(200).json(info);
+    }catch(e){
+        res.status(500).json({error: e.message})
+    }
+}
+
+// get single info
+const getSingleInfo = async (req,res) => {
+    try{
+        const {id} = req.params;
+        const info = await infoModel
+        .findById(id);
+        res.status(200).json(info);
+    }catch(e){
+        res.status(500).json({error: e.message})
+    }
+}
+
+// update info
+const updateInfo = async (req,res) => {
+    try{
+        const {id} = req.params;
+        const info = await infoModel
+        .findByIdAndUpdate(id, req.body
+        , {new: true});
+        res.status(200).json(info);
+    }catch(e){
+        res.status(500).json({error: e.message})
+    }
+}
+
+// delete info
+const deleteInfo = async (req,res) => {
+    try{
+        const {id} = req.params;
+        const info = await infoModel
+        .findByIdAndDelete(id);
+        res.status(200).json(info);
+    }catch(e){
+        res.status(500).json({error: e.message})
+    }
+}
+
+
+
+module.exports = {createInfo, getAllInfo, getSingleInfo, updateInfo, deleteInfo};
--- backend/routes/InfoRoute.js
+++ backend/routes/InfoRoute.js
@@ -1,8 +1,12 @@
 const express =require('express');
 const router = express.Router();
 
-const CreateInfo = require('../controller/InfoController');
+const {createInfo , getAllInfo, getSingleInfo, updateInfo, deleteInfo} = require('../controller/InfoController');
 
-router.post('/createinfo', CreateInfo);
+router.post('/create', createInfo);
+router.get('/get-all', getAllInfo);
+router.get('/get-single/:id', getSingleInfo);
+router.put('/update/:id', updateInfo);
+router.delete('/delete/:id', deleteInfo);
 
 module.exports = router;
