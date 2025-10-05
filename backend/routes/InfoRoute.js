9499d125ce7e84ff0281e2137591ca24f77ad49d
Ashokan <ashokankuganathan@gmail.com>
2025-10-05 18:43:49 +0530

    started to work on frontend

--- backend/routes/InfoRoute.js
+++ backend/routes/InfoRoute.js
@@ -1,5 +1,5 @@
 const express =require('express');
-const router = express.Router();
+const router = express.Router(); // express router for routing
 
 const {createInfo , getAllInfo, getSingleInfo, updateInfo, deleteInfo} = require('../controller/InfoController');
 
--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,28 @@
-const express = require('express');
-const app = express();
-require('dotenv').config();
-const mongoose = require('mongoose');
+const express = require('express'); //import express for creating server
+const app = express(); // create app using express for server
+require('dotenv').config(); //for using .env file
+const mongoose = require('mongoose'); //import mongoose for connecting to mongoDB
 const infoRoute = require('./routes/InfoRoute');
+const cors = require('cors');
 
-app.use(express.json());
-
-app.get('/',function(req,res){
+app.use(express.json()); // to accept json data
+app.use(cors());
+app.get('/', function (req, res) {
     res.send('Ashokan is on 🔥❤️‍🔥')
 })
 
-//middleware
-// app.use((req,res,next) => {
-//     console.log(req.path, req.method);
-//     next();
-// })
-
 mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-    app.listen(process.env.PORT ,() => {
-        console.log ("Connected to MongoDB : " + process.env.PORT );
-    });
-})
-.catch((error) => console.log(error));
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log("Connected to MongoDB : " + process.env.PORT);
+        });
+    })
+    .catch((error) => console.log(error));
 
-app.use('/api/info',infoRoute);
+app.use('/api/info', infoRoute);
 
-
+app.get('/api/data', (req, res) => {
+    const data = [{ id: 1, name: 'Item 1' }, { id: 2, name: 'Item 2' }];
+    res.json(data);
+});
 
